e6c3c92916fc51839afef207c4496663cb9636cf
sasamafi5-cloud <sasamafi5@googlemail.com>
2026-05-19 07:36:37 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = '3d-neumorphic-player-v2';
+const ASSETS = [
+  './',
+  './index.html'
+];
+
+// Instalacija i keširanje aplikacije
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    }).then(() => self.skipWaiting())
+  );
+});
+
+// Aktivacija i čišćenje starih keševa
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Presretanje zahteva i rad u offline režimu
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
